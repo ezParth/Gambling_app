0e3aa4ee85c217bb84171de9fe348b2dfe069361
ezParth <parthsingh55m@gmail.com>
2025-05-18 11:03:39 +0530

    auth

---
+++ server/config/Response.ts
@@ -0,0 +1,62 @@
+// Response.ts
+class NullResponse {
+    statusCode: number;
+    message: string;
+    success: boolean;
+
+    constructor(statusCode: number, message: string, success: boolean) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.success = success;
+    }
+
+    SendResponse(res: any) {
+        return res.status(this.statusCode).json({
+            message: this.message,
+            success: this.success,
+        });
+    }
+}
+
+class ValueResponse {
+    statusCode: number;
+    message: string;
+    success: boolean;
+    data: { [key: string]: any };
+
+    constructor(statusCode: number, message: string, key: string, value: any, success: boolean) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.success = success;
+        this.data = { [key]: value };
+    }
+
+    SendResponse(res: any) {
+        return res.status(this.statusCode).json({
+            message: this.message,
+            success: this.success,
+            ...this.data,
+        });
+    }
+}
+
+class CustomError {
+    statusCode: number;
+    message: string;
+    success: boolean;
+
+    constructor(statusCode: number, message: string) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.success = false;
+    }
+
+    SendResponse(res: any) {
+        return res.status(this.statusCode).json({
+            message: this.message,
+            success: this.success,
+        })
+    }
+}
+
+module.exports = { NullResponse, ValueResponse, CustomError };
--- server/controller/user.controller.ts
+++ server/controller/user.controller.ts
@@ -1,13 +1,76 @@
-import User from "../model/user.model"
+import asyncHandler from "express-async-handler";
+import User from "../model/user.model";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+const { NullResponse, ValueResponse, CustomError } = require('./Response');
 
-const login = (req:any, res:any) => {
+const SECRET = 'helloworld';
+
+const login = asyncHandler(async (req: any, res: any) => {
     const { username, password } = req.body;
-    if(!username || !password) {
-        return res.status(400).json({
-            message: "Please provide username and password",
-            success: false,
-        })
+
+    if (!username || !password) {
+        return new NullResponse(400, "Please provide username and password", false).SendResponse(res);
     }
 
-    const user = 
-}
+    const user = await User.findOne({ username });
+    if (!user) {
+        return new NullResponse(404, "User not found", false).SendResponse(res);
+    }
+
+    const isPasswordValid = bcrypt.compareSync(password, user.password);
+    if (!isPasswordValid) {
+        return new NullResponse(401, "Invalid credentials", false).SendResponse(res);
+    }
+
+    const token = jwt.sign(
+        { id: user._id, username: user.username },
+        SECRET,
+        { expiresIn: "7d" }
+    );
+
+    const userInfo = {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        token
+    };
+
+    return new ValueResponse(200, "Login successful", "user", userInfo, true).SendResponse(res);
+});
+
+const signin = asyncHandler(async (req: any, res: any) => {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+        return new CustomError(400, "Please provide all the information").SendResponse(res);
+    }
+
+    const userExists = await User.findOne({ username });
+    if (userExists) {
+        return new NullResponse(409, "User already exists", false).SendResponse(res);
+    }
+
+    const hashedPassword = bcrypt.hashSync(password, 10);
+
+    const newUser = await User.create({
+        username,
+        email,
+        password: hashedPassword
+    });
+
+    const token = jwt.sign(
+        { id: newUser._id, username: newUser.username },
+        SECRET,
+        { expiresIn: "7d" }
+    );
+
+    const userInfo = {
+        id: newUser._id,
+        username: newUser.username,
+        email: newUser.email,
+        token
+    };
+
+    return new ValueResponse(201, "User created successfully", "user", userInfo, true).SendResponse(res);
+});
