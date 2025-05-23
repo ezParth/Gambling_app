fd794faad6d4874c11eb6e2b57b97c2555686f25
ezParth <parthsingh55m@gmail.com>
2025-05-23 16:40:58 +0530

    Added odiRanking backend generator

---
+++ server/controller/odiRanking.controller.ts
@@ -0,0 +1,22 @@
+import odiRankings from '../data/IccBatsmanRanking.json'
+
+const generateOdiRanking = () => {
+    try {
+        let result = [];
+        const batsmen = odiRankings.odi;
+
+        result = batsmen.map(b => ({
+            batsman: b.name,
+            country: b.country,
+            rank: b.rank,
+            rating: b.rating
+        }));
+
+        console.log(result);
+        return result; // if you want to return the result
+    } catch (error) {
+        console.log("Error during generating odi ranking", error);
+    }
+}
+
+export { generateOdiRanking }
